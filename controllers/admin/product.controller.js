986a4aeb44f7325290d40694a58ca56bbbeeb27e
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-08-25 19:16:06 +0700

    Buoi 1: Lay ra danh sach san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,15 @@
+const Product = require('../../models/product.model')
+
 //[GET] /admin/products
-module.exports.product = (req, res) => {
+module.exports.product = async (req, res) => {
+
+    const products = await Product.find({})
+
+    console.log(products);
+
     res.render("admin/pages/products/index", {
-        titlePage: "Quản lý sản phẩm"
+        titlePage: "Quản lý sản phẩm",
+        products: products
+
     })
 }
