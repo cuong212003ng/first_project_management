5878de106fee9369f4c01aad748e05de03820e35
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-08-25 18:50:04 +0700

    Buoi 1: Them phan layout chung cua ADMIN

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ database.connect()
 const clientRoute = require('./routes/client/index.route')
 const adminRoute = require('./routes/admin/index.route')
 
+const systemConfig = require('./config/system')
+
 const app = express()
 const port = process.env.PORT
 
@@ -14,6 +16,9 @@ app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+//App locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
 
 //Routes  
 clientRoute(app)
