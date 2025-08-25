36b87e321655e9159c7c9a933913ae4ea55c5410
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-08-25 11:12:48 +0700

    Buoi 1: Them boostrap 5 va public file tinh

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const route = require('./routes/client/index.route')
 const app = express()
 const port = process.env.PORT
 
+app.use(express.static('public'))
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 
