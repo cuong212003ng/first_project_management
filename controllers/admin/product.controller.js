4e0d2c907a2d52063dc87693723d712826f57c3e
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-08-27 17:09:42 +0700

    Buoi 2: Code tinh nang Bo Loc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,13 +3,55 @@ const Product = require('../../models/product.model')
 //[GET] /admin/products
 module.exports.product = async (req, res) => {
 
-    const products = await Product.find({})
+    // console.log(req.query.status);
+    
 
-    console.log(products);
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            value: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            value: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            value: "inactive",
+            class: ""
+        }
+    ]
+
+
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.value == req.query.status)
+        filterStatus[index].class = "active"
+    } else {
+        const index = filterStatus.findIndex(item => item.value == "")
+        filterStatus[index].class = "active"
+    }
+    let find = {
+        deleted: false
+    }
+
+    if (req.query.status) {
+        find.status = req.query.status
+    }
+
+    if (req.query.price) {
+        find.price = req.query.price
+    }
+
+    const products = await Product.find(find)
+
+    // console.log(products);
 
     res.render("admin/pages/products/index", {
         titlePage: "Quản lý sản phẩm",
-        products: products
+        products: products,
+        filterStatus: filterStatus
 
     })
 }
