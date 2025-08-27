8a89e62ffc6966a36bc284038d1f548034d45147
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-08-27 17:22:32 +0700

    Buoi 2: Code chuc nang Tim Kiem

--- public/admin/js/admin.js
+++ public/admin/js/admin.js
@@ -26,7 +26,28 @@ if(buttonStatus.length > 0){
 
 //End Button Status
 
+//Form Search
+const formSearch = document.querySelector("#form-search")
+if(formSearch){
+    let url = new URL(window.location.href)
 
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        console.log(e.target.keyword.value);
+
+        const keyword = e.target.keyword.value
+        if(keyword) {
+            url.searchParams.set("keyword", keyword)
+        } else {
+            url.searchParams.delete("keyword")
+        }
+        
+        window.location.href = url.href
+    })
+}
+
+//End Form Search
 
 
 
